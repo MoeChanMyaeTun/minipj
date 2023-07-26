14d869fdae04a4240bd7bba5d03da70b55d0957b
swesweaung-scm <scm.swesweaung@gmail.com>
2023-07-26 15:29:05 +0630

    7/26/2023(install nuxt2, tailwind, laravel)

---
+++ client/pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+   <h1 class="text-3xl font-bold underline">
+        Hello world!
+        {{kitchen}}
+      </h1>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage',
+   async asyncData({$axios}){
+  const kitchen = await $axios.$get('/kitchen')
+  console.log(kitchen)
+   return {kitchen}
+ }
+}
+</script>
